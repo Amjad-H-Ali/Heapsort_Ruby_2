9bcbf51324ecfca0760cc85b69eac43edcca3496
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-01-03 00:15:07 -0600

    If left child is greater than parent, set largest variable equal to its index

--- array-to-tree.rb
+++ array-to-tree.rb
@@ -9,4 +9,10 @@ def max_heap array, indx, size
 	# Left and right children nodes' index
 	left  = (indx * 2) + 1
 	right = left + 1
+
+	# If left child is greater than parent, set largest variable equal to its index
+	if left < size and array[left] > array[largest]
+		largest = left
+	end
+		
 end	
