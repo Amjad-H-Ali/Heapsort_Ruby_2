391676e11d9cdcf65995438a4cbb325323b4e9c6
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-01-03 00:00:39 -0600

    Defined and initialized largest variable

--- array-to-tree.rb
+++ array-to-tree.rb
@@ -3,5 +3,7 @@ p "Array To Tree Ruby"
 # Function that arranges given array into max heap
 # meaning all parent nodes are greater than children
 def max_heap array, indx, size
-
+	# Default value of largest indx is parent indx
+	largest = indx
+	
 end	
